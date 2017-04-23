d30ee6f621e430d3165972fde49695b81c251a80
martinchu <martinchu09@gmail.com>
2017-04-23 19:13:18 +0100

    set up new Vuex file to migrate all data

--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,34 @@ Vue.use(Vuex);
 
 
 export default new Vuex.Store({
-  state: {
-
+  state:{
+    size: 8,
+    liveCells:[],
+    deadCells:[],
+    initialState:[]
   },
   mutations: {
-
+    toggleCell(state,payload){
+      Vue.set(state.initialState[payload.rowIndex],payload.columnIndex,!state.initialState[payload.rowIndex][payload.columnIndex])
+      var originalCellArray;
+      var newCellArray;
+      // if this cell is now alive(being populated), push the cell value into the liveCells array and delete it off the deadcells array
+      if(state.initialState[payload.rowIndex][payload.columnIndex]){
+        newCellArray = state.liveCells;
+        originalCellArray = state.deadCells;
+      }
+      // if this cell is now alive(being populated), push the cell value into the deadCells array and delete it off the liveCells array
+      else{
+        newCellArray = state.deadCells;
+        originalCellArray = state.liveCells;
+      }
+      newCellArray.push(payload);
+      originalCellArray.forEach((originalCell,i) =>{
+        if(payload.columnIndex == originalCell.columnIndex && payload.rowIndex == originalCell.rowIndex){
+          originalCellArray.splice(i,1) //splice the cell is proven to ve not working
+        }
+      })
+    }
   },
   actions:{
 
